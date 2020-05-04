c5953533e08c90a351ec769909b669f8e126c6ad
ye cai <yushunchn@gmail.com>
2020-05-04 13:24:34 +0800

    Updates

---
+++ precache-manifest.391061f966fe31653642f6e80248d5fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6210901c6f783eb6dc392181145935",
+    "url": "/studying-redux/events-demo/index.html"
+  },
+  {
+    "revision": "3e4bf524278a3e06a100",
+    "url": "/studying-redux/events-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f4693db28db20f3f1c77",
+    "url": "/studying-redux/events-demo/static/js/2.c3f49628.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/studying-redux/events-demo/static/js/2.c3f49628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4bf524278a3e06a100",
+    "url": "/studying-redux/events-demo/static/js/main.343c0b9e.chunk.js"
+  },
+  {
+    "revision": "0969751129d8884c01b4",
+    "url": "/studying-redux/events-demo/static/js/runtime-main.8689d7dc.js"
+  }
+]);
